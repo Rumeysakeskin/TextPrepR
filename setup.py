c026840c0b5225c14de2d56c6bc7f254ba91a187
Rumeysakeskin <61134521+Rumeysakeskin@users.noreply.github.com>
2022-09-30 16:42:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,4 +13,4 @@ setuptools.setup(name='TextPrepR',
                  classifiers=['Programming Language:: Python :: 3',
                                'License :: OSI Aproved :: MIT License'
                                'Operating System:: OS Independent'],
-                               python_requires = ">=3.8")
+                               python_requires = ">=3.6")
